ef21a3fdb29fd7a4f74f0da881c95153eabdab7c
mertoatakan <mertoatakan@gmail.com>
2024-03-10 02:05:40 +0300

    initial commit

---
+++ logging-service/src/test/java/com/mertoatakan/loggingservice/LoggingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mertoatakan.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
