61a7ee7c51a9a577f5ae769eaa5d7a62cd394424
mertoatakan <mertoatakan@gmail.com>
2024-03-10 02:07:45 +0300

    Add BaseEntityService

---
+++ user-service/src/main/java/com/mertoatakan/userservice/general/BaseEntityService.java
@@ -0,0 +1,55 @@
+package com.mertoatakan.userservice.general;
+
+import com.mertoatakan.userservice.exceptions.ItemNotFoundException;
+import lombok.Getter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Getter
+public abstract class BaseEntityService<E extends BaseEntity, R extends JpaRepository<E, Long>>{
+
+    private final R repository;
+
+    protected BaseEntityService(R repository) {
+        this.repository = repository;
+    }
+
+    public E save(E entity){
+        BaseAdditionalFields baseAdditionalFields = entity.getBaseAdditionalFields();
+        if (baseAdditionalFields == null){
+            baseAdditionalFields = new BaseAdditionalFields();
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        if (entity.getId() == null){
+            baseAdditionalFields.setCreatedDate(now);
+        }
+        baseAdditionalFields.setUpdateDate(now);
+        entity.setBaseAdditionalFields(baseAdditionalFields);
+
+        entity = repository.save(entity);
+        return entity;
+    }
+
+    public Optional<E> findById(Long id){
+        return repository.findById(id);
+    }
+
+    public E findByIdWithControl(Long id){
+        Optional<E> optionalE = repository.findById(id);
+        E entity;
+        if (optionalE.isPresent()){
+            entity = optionalE.get();
+        }
+        else{
+            throw new ItemNotFoundException(GeneralErrorMessage.ITEM_NOT_FOUND);
+        }
+        return entity;
+    }
+
+    public boolean existById(Long id){
+        return repository.existsById(id);
+    }
+}
