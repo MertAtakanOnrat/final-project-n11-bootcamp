e15589470c21111de5b3ea4938c808d19872a652
mertoatakan <mertoatakan@gmail.com>
2024-03-12 10:34:35 +0300

    Refactor GeneralControllerAdvice of user-service

--- user-service/src/main/java/com/mertoatakan/userservice/general/GeneralControllerAdvice.java
+++ user-service/src/main/java/com/mertoatakan/userservice/general/GeneralControllerAdvice.java
@@ -1,9 +1,12 @@
 package com.mertoatakan.userservice.general;
 
+import com.mertoatakan.userservice.service.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
-import org.springframework.validation.FieldError;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,37 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ControllerAdvice
 @RestController
+@RequiredArgsConstructor
 public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Map<String, String>> errorList = ex.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).toList();
+
+        String description = request.getDescription(false);
+        var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), errorList.toString(), description);
+        var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage("errorLog", generalErrorMessages.message());
+
+        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler
     public final ResponseEntity<Object> handleRTExceptions(Exception e, WebRequest request){
@@ -28,25 +56,34 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         RestResponse<GeneralErrorMessages> restResponse = RestResponse.error(generalErrorMessages);
 
+        kafkaProducerService.sendMessage("errorLog", message);
+
         return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
-        Map<String, String> validationErrors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            validationErrors.put(fieldName, errorMessage);
-        });
 
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleAllExceptions(TransactionSystemException e, WebRequest request) {
+        String rawMessage = e.getOriginalException().getCause().getMessage();
         String description = request.getDescription(false);
-        GeneralErrorMessages generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), "Validation Failed", description);
-        RestResponse<GeneralErrorMessages> restResponse = RestResponse.error(generalErrorMessages);
 
-        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
+        Pattern pattern = Pattern.compile("messageTemplate='(.*?)'");
+        Matcher matcher = pattern.matcher(rawMessage);
+
+        List<String> extractedMessages = new ArrayList<>();
+        while (matcher.find()) {
+            extractedMessages.add(matcher.group(1));
+        }
+
+        String processedMessage = String.join(", ", extractedMessages);
+
+        var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), processedMessage, description);
+        var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage("errorLog", processedMessage);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 
 }
